530b8a32a5cd84c860f19755e743aea9e1d6048e
multidatapack@hotmail.com <multidatapack@hotmail.com>
2024-09-20 20:54:27 -0700

    test

---
+++ src/main/java/net/west/fantasymod/FantasyModClient.java
@@ -0,0 +1,10 @@
+package net.west.fantasymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FantasyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/west/fantasymod/FantasymodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.west.fantasymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FantasymodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
